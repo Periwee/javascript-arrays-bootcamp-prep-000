29f762f66ba15654fad3b716aa581ed9f18f7a62
Esperanza Rodriguez <esperanza.rguezz@gmail.com>
2019-03-31 20:38:33 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -20,6 +20,6 @@ function accessElementInArray(array, index){
   return array[index]
 }
 
-function estructivelyRemoveElementFromBeginningOfArray(array){
+function destructivelyRemoveElementFromBeginningOfArray(array){
   return array.shift()
 }
